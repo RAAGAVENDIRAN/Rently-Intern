71f0b18f256963cc3b8681cb201c56ff77b28510
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-03 16:25:59 +0530

    eight-commit

--- src/index.js
+++ src/index.js
@@ -2,49 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Reactform extends React.Component{
-  constructor(){
-    super();
-    this.state={username:"",
-                age:null,
-                errmsg:""};
-  }
 
 
-  uservalue=(event)=>{ //event is passed as arguement. automatically captures what the event given. here it is onchange
-    let n=event.target.name;  // what name they have selected eg:username or age
-    let v=event.target.value; // the respective values are here.
-    let err="";
-    if (n==="age"){
-      if(v!="" && !Number(v)){
-        err=<strong>Invalid value, Your age must be a Number</strong>
-      }
-    }
-    this.setState({errmsg:err}); //event.target targets the value given in field
-    this.setState({[n]:v});
-
-  }   //can use same function for multiple input tags.
-
-
-
-  formSubmit=(event)=>{
-    event.preventDefault();  // preventDefault: in alert msg the entered values eill be shown, to check it is useful
-    alert("Your Name: "+ this.state.username);
-    
-  }
-
-  render(){
-    return(
-     <form onSubmit={this.formSubmit}>
-      <h1>Hello {this.state.username}</h1>
-      <h1>Your age {this.state.age}</h1>
-      Enter Your Name: <input type="text" name="username" onChange={this.uservalue}/><br/>
-      Enter Your Age: <input type="text" name="age" onChange={this.uservalue}/>   {this.state.errmsg}
-      <input type="submit"/>
-     </form>
-    )
-  }
+function Menubar(){
+  const newlist=[1,2,3,4,5]
+  const r1=newlist.map((listvalues) =>{  //in arrow function we are mapping values one by one 
+    return <li>{listvalues}</li>
+  });
+  return  <ul>{r1}</ul>
 }
 
-const root= ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Reactform/>);
+
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Menubar newlist/>);
