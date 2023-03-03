3a4a76c9a77152608ffd0a224e0663a0e4c18446
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-03 11:22:56 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ import ReactDOM from 'react-dom';
 
 
 
-class ReactState extends React.Component{
-  constructor() { //state can be  initialized only in constructor.
-      super();   //super keyword is used to initize values and that values can be called in other methods.
-      this.state={initialvalue:"Welcome",name:"Guys"}   //creating state
+  class ReactState extends React.Component{
+    constructor() { //state can be  initialized only in constructor.
+        super();   //super keyword is used to initize values and that values can be called in other methods.
+        this.state={initialvalue:"Welcome",name:"Guys"}   //creating state
 
-  }
-  render() {  // render method is to display in webpage
-  
-          return <div>
-                <h1>{this.state.initialvalue } {this.state.name}</h1>
-                <button type="button"> onClick={this.changeValue}</button>
-          </div> 
-  
-  }
-  }
+    }
+
+    changevalue=()=>  { //arrow function
+          this.setState({initialvalue:"Thanks for changing"})
+    }
+    render() {  // render method is to display in webpage
+    
+            return <div>
+                  <h1>{this.state.initialvalue } {this.state.name}</h1>
+                  <button type="button" onClick={this.changevalue}> ChangeValue</button>
+            </div> 
+    
+    }
+    }
 
 
-  const root=ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<ReactState/>);
+    const root=ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<ReactState/>);
