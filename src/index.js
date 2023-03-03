e51c268510796c5a678bcb33f97d580fc8de7237
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-03 14:23:34 +0530

    sixth-commit

--- src/index.js
+++ src/index.js
@@ -2,63 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-
-  class LifeCycle extends React.Component{
-    constructor() { //state can be  initialized only in constructor.
-        super();   //super keyword is used to initize values and that values can be called in other methods.
-        this.state={value:"Welcome",name:"Guys"}   //creating state
-
+class Eventbind extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      msg:"welcome"
     }
+  }
 
-    componentWillMount(){  // comes before render.comes only on first time page is rendered
-      alert("Leran ReactJs Lifecyle");
-    }
+  clickEvent=(a)=>{
+    this.setState({
+      msg:a
+    })
+  }
 
-    changevalue = () =>{
-          this.setState({value:"Modified Value"})  //updating new Value
-    }
-
-
-    deleteheader=()=>{
-      this.setState({value:false,name:false})
-    }
-    render(){
-         return <div>
-          <h1>{this.state.value} {this.state.name}</h1>
-          <br/>
-          <button type="button" onClick={this.changevalue}>ChangeValue</button>
-          <button type="button" onClick={this.deleteheader}>Deleteheader</button>
-          </div>
-    }
-
-    componentDidMount(){  // works after rendering
-      setTimeout(()=>{
-        this.setState({value:"ThankYou"})},5000)
-       
-    }
-
-    componentWillUpdate(){  //invoked whaenever a new update of value is there.
-      alert("Do you want update a new Value");
-    }
-
-    componentDidUpdate(){  // invoked if componentWillUpdate is invokes
-      document.getElementById("mydiv").innerHTML="New value updated successfully "+this.state.value;
-    }
-    
-    shouldComponentUpdate(){
-      return true;    //if given false no updats will be carrried out.
-    }
-
-    componentWillUnmount(){  //to delete
-          alert("Header");
-    }
-
-
-
+  render(){
+    return (<div>
+      <h1>{this.state.msg}</h1>
+      <button onClick={()=>this.clickEvent("Hello World ")}>Click</button>
+    </div>
+    )
+  }
 }
-    
 
 
-    const root=ReactDOM.createRoot(document.getElementById('root'));
-    root.render(<LifeCycle/>);
+
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Eventbind/>);
